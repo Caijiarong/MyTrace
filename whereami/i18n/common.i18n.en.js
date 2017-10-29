e553510ccc131b4ca7854fdef3b266f8fd7e590a
coolspring1293 <coolspring1293@outlook.com>
2017-10-29 14:03:38 +0800

    delete old version

--- whereami/i18n/common.i18n.en.js
+++
@@ -1,3 +0,0 @@
-define({
-    xxx:"en"
-});
--- whereami/main.js
+++
@@ -1,9 +0,0 @@
-define(['modules/main/views/IndexView'], function(IndexView) {
-    
-	fish.View.configure({manage: true});
-
-	// 这里配置一下语言，可以动态设置，真实的系统需要根据浏览器设置来获取语言然后设置给fish,默认语言是en
-	fish.setLanguage('en');
-
-	new IndexView().render();
-});
